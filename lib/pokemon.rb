2a4edf8ae282bd3fb275f87bc7fe7dfc09811857
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-26 17:46:34 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ require_relative "../bin/environment"
 class Pokemon
 attr_accessor :name, :type, :id, :db
 
-  def initialize(pk_name:, pk_type:, id = nil)
+  def initialize(pk_name:, pk_type:)
     @name = pk_name
     @type = pk_type
   end
