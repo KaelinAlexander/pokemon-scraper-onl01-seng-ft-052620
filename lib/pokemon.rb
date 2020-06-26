510b19b5efe0cb323e85d59946617bf551ccfa92
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-26 17:51:38 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,6 +20,7 @@ attr_accessor :name, :type, :id, :db
       SQL
       DB[:conn].execute(sql, self.name, self.type)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+      binding.pry
     end
   end
 
