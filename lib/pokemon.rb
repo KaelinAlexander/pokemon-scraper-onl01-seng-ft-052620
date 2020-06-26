dc020f5e0847a388bb0e122826efb3a14f5f84d8
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-26 17:31:15 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
 attr_accessor :name, :type, :id, :db
 
-  def initialize(pk_name:, pk_type:)
+  def initialize(pk_name:, pk_type:, id: = nil)
     @name = pk_name
     @type = pk_type
   end
@@ -11,7 +11,13 @@ attr_accessor :name, :type, :id, :db
       self.update
     else
       sql = <<-SQL
-
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.type)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end 
+  end
 
   end
 
