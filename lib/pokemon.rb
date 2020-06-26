dd1fea19a0bfe94a9174ed5d99df7c955203efaa
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-26 17:14:33 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,6 +2,13 @@ class Pokemon
 attr_accessor :pk_name, :pk_type
 
   def initialize(pk_name, pk_type)
+    @pk_name = pk_name
+    @pk_type = pk_type
+  end
+
+  def self.all
+
+  end
 
   def save
 
