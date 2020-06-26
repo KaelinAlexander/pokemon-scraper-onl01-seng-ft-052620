b40a747a2230d2f99206994d81bae641247d2cbd
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-26 17:40:47 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,6 +19,11 @@ attr_accessor :name, :type, :id, :db
     end
   end
 
+  def self.create(name, type)
+    pk_name: = name
+    pk_type: = type
+    new_pokemon = self.new(pk_name:, pk_type:)
+
   def self.new_from_db(row)
     id: = row[0]
     pk_name: = row[1]
